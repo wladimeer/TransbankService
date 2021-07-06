d4fbe03fddbd11ffb17950e00e633e51ccd41719
Sebastián <wladimeerbc@gmail.com>
2021-07-06 12:35:03 -0400

    Method Create Finished

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const Express = require('express');
-const Transbank = require('transbank-sdk').WebpayPlus;
+import Express, { json } from 'express';
+import { WebpayPlus as Transbank } from 'transbank-sdk';
 
 const app = Express();
 app.set('port', process.env.PORT || 3000);
-app.use(Express.json());
+app.use(json());
 
-app.all('/', (request, response, next) => {
-  next();
-});
+// app.all('/', (request, response, next) => {
+//   next();
+// });
 
 app.post('/create', (request, response) => {
   let buyOrder = `O${String(Math.floor(Math.random() * 9999999))}`;
@@ -23,6 +23,50 @@ app.post('/create', (request, response) => {
       response.json(value);
     });
 });
+
+app.post('/status', (request, response) => {
+  const token = request.body.token;
+
+  Transbank.Transaction.status(token)
+    .then((value) => {
+      switch (value.vci) {
+        case 'TSY':
+          CommitTransaction(token)
+            .then((value) => {
+              response.json(value);
+            })
+            .catch((value) => {
+              response.json(value);
+            });
+          break;
+        case 'TSN':
+          response.json('El Pago no Pudo Ser Completado!');
+          break;
+      }
+    })
+    .catch((value) => {
+      response.json('Hubo un Error al Realizar el Pago!');
+    });
+});
+
+const CommitTransaction = (token) => {
+  return new Promise((resolve, reject) => {
+    Transbank.Transaction.commit(token)
+      .then((response) => {
+        switch (response.status) {
+          case 'AUTHORIZED':
+            resolve('Pago Realizado Correctamente!');
+            break;
+          case 'FAILED':
+            reject('La Tarjeta Ingresada fue Declinada!');
+            break;
+        }
+      })
+      .catch((response) => {
+        reject('Hubo un Error al Realizar el Pago!');
+      });
+  });
+};
 
 app.listen(app.get('port'), () => {
   console.log(`Service Starting on Port ${app.get('port')}`);
