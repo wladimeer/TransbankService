255abc186cb195b7dc153837450b2c9c725554f0
Sebastián <wladimeerbc@gmail.com>
2021-07-06 11:01:36 -0400

    Structure Finished

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.all('/', (request, response, next) => {
 app.post('/create', (request, response) => {
   const purchase = request.body.purchase;
   // response.send(purchase);
-  response.json(purchase);
+  response.json({ name: 'purchase' });
 });
 
 app.listen(app.get('port'), () => {
